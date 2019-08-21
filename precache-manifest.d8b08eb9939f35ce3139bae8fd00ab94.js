a73be23358dca5f3f02b6da633c372dcc7de32ae
XuanNam <doremon22100@gmail.com>
2019-08-21 20:41:58 +0700

    Updates

---
+++ precache-manifest.d8b08eb9939f35ce3139bae8fd00ab94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbb9c586831dec4d7876947ec3f21c1",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "16063d42d33b437d4417",
+    "url": "/react-shopping-cart/static/css/main.59c8f9f5.chunk.css"
+  },
+  {
+    "revision": "628683cd3d7e67eb8eff",
+    "url": "/react-shopping-cart/static/js/2.9cdb533c.chunk.js"
+  },
+  {
+    "revision": "16063d42d33b437d4417",
+    "url": "/react-shopping-cart/static/js/main.31223f96.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
